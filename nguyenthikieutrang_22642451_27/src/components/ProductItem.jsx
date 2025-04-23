7b54d129f471a31a8c14b88713a4d0f195c708e5
H82M19\Student <Student>
2025-04-23 18:59:52 +0700

    lưu và tải sản phẩm từ localStorage

--- nguyenthikieutrang_22642451_27/src/components/ProductItem.jsx
+++ nguyenthikieutrang_22642451_27/src/components/ProductItem.jsx
@@ -1,21 +1,17 @@
-const ProductItem = ({ product, index, onDelete }) => {
+// src/components/ProductItem.jsx
+function ProductItem({ product, onDelete }) {
     return (
       <tr>
-        <td className="py-1 px-4 border">{product.name}</td>
-        <td className="py-1 px-4 border">{product.price.toLocaleString()}₫</td>
-        <td className="py-1 px-4 border">{product.category}</td>
-        <td className="py-1 px-4 border">{product.stock}</td>
-        <td className="py-1 px-4 border">
-          <button
-            onClick={() => onDelete(index)}
-            className="bg-red-500 text-white px-2 py-1 rounded text-xs"
-          >
-            Xoá
-          </button>
+        <td>{product.name}</td>
+        <td>{product.price.toLocaleString()} VND</td>
+        <td>{product.category}</td>
+        <td>{product.stock}</td>
+        <td>
+          <button onClick={onDelete}>Xoá</button>
         </td>
       </tr>
     );
-  };
+  }
   
   export default ProductItem;
   
